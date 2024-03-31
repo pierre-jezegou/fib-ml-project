d1805c1be5531335aef012d2796d2e6d559f2b6e
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-03-31 19:37:37 +0200

    Add datasources and dataset algo

---
+++ build_dataframe.py
@@ -0,0 +1,46 @@
+'''Build dataset'''
+import pandas as pd
+
+FILENAMES = ["data/gares-de-voyageurs.csv",
+             "data/frequentation-gares.csv",
+            "data/gares-pianos.csv",
+            "data/gares-equipees-du-wifi.csv",
+            ]
+
+def transform_uid(uic: int | float | str) -> str:
+    '''Format UIC'''
+    return str(uic).split(".")[0].zfill(10)
+
+### TRANSFORMATIONS
+# Transform frequentation in train stations
+frequentation = pd.read_csv('data/frequentation-gares-raw.csv', delimiter=";", header=0)
+frequentation["UIC"] = frequentation["UIC"].astype(str).str.zfill(10)
+frequentation = frequentation.rename(columns={'Nom de la gare': 'Gare'})
+
+frequentation.to_csv('data/frequentation-gares.csv', index=False, sep=";")
+
+
+# Transform wifi presence in train stations
+wifi = pd.read_csv('data/gares-equipees-du-wifi-raw.csv', delimiter=";", header=0)
+wifi["UIC"] = wifi["UIC"].apply(lambda uid: ("87"+str(uid).split(".")[0]).zfill(10))
+wifi.to_csv('data/gares-equipees-du-wifi.csv', index=False, sep=";")
+
+
+
+def merge_data_with_key(filenames: list[str]) -> None:
+    '''Merge datasets'''
+    result = pd.DataFrame()
+    for filename in filenames:
+        if filename is not None:
+            if result.empty:
+                result = pd.read_csv(filename, header=0, delimiter=';')
+            else:
+                data_pd = pd.read_csv(filename, header=0, delimiter=';')
+                data_pd = data_pd.drop(columns=['Gare'])
+                data_pd["UIC"] = data_pd["UIC"].apply(transform_uid)
+                result = pd.merge(result, data_pd, on="UIC", how="left")
+
+    result.to_csv('data/sncf_dataset.csv', sep=";")
+
+
+merge_data_with_key(FILENAMES)
