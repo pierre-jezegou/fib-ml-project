93f72e9624abc886c8dd20dcb5e91dd87696684a
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-04-17 23:05:06 +0200

    Modifications  following feedback on the first version

--- build_dataframe.py
+++ build_dataframe.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 FILENAMES = ["data/gares-de-voyageurs.csv",
              "data/frequentation-gares.csv",
-            "data/gares-pianos.csv",
-            "data/gares-equipees-du-wifi.csv",
-            ]
+             "data/gares-pianos.csv",
+             "data/gares-equipees-du-wifi.csv",
+             ]
 
 def transform_uid(uic: int | float | str) -> str:
     '''Format UIC'''
-    return str(uic).split(".")[0].zfill(10)
+    return str(uic).split(".", maxsplit=1)[0].zfill(10)
 
 ### TRANSFORMATIONS
 # Transform frequentation in train stations
@@ -22,7 +22,7 @@ frequentation.to_csv('data/frequentation-gares.csv', index=False, sep=";")
 
 # Transform wifi presence in train stations
 wifi = pd.read_csv('data/gares-equipees-du-wifi-raw.csv', delimiter=";", header=0)
-wifi["UIC"] = wifi["UIC"].apply(lambda uid: ("87"+str(uid).split(".")[0]).zfill(10))
+wifi["UIC"] = wifi["UIC"].apply(lambda uid: ("87"+str(uid).split(".", maxsplit=1)[0]).zfill(10))
 wifi.to_csv('data/gares-equipees-du-wifi.csv', index=False, sep=";")
 
 
@@ -45,15 +45,4 @@ def merge_data_train_stations(filenames: list[str]) -> pd.DataFrame:
 
 # Final merge
 train_stations = merge_data_train_stations(FILENAMES)
-prices = pd.read_csv('data/tarifs-tgv-inoui-ouigo.csv', delimiter=";", header=0)
-
-departure_stations = train_stations.add_prefix("departure_")
-arrival_stations = train_stations.add_prefix("arrival_")
-
-
-prices["Gare origine - code UIC"] = prices["Gare origine - code UIC"].apply(transform_uid)
-prices["Gare destination - code UIC"] = prices["Gare destination - code UIC"].apply(transform_uid)
-
-prices_consilidated = prices.merge(departure_stations, how="left", left_on="Gare origine - code UIC", right_on="departure_UIC").merge(arrival_stations, how="left", left_on="Gare destination - code UIC", right_on="arrival_UIC")
-
-prices_consilidated.to_csv("sncf_dataset.csv", sep=";")
+train_stations.to_csv("sncf_train_stations_dataset.csv", sep=";")
