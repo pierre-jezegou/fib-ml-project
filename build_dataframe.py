dfa8482c5db0024e687ddb4dc1bbc0b84f8141d5
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-05-09 18:47:57 +0200

    Add cities information consolidation - Population in attraction areas

--- build_dataframe.py
+++ build_dataframe.py
@@ -1,31 +1,32 @@
 '''Build dataset'''
 import pandas as pd
 
-FILENAMES = ["data/gares-de-voyageurs.csv",
-             "data/frequentation-gares.csv",
-             "data/gares-pianos.csv",
-             "data/gares-equipees-du-wifi.csv",
-             ]
 
 def transform_uid(uic: int | float | str) -> str:
     '''Format UIC'''
     return str(uic).split(".", maxsplit=1)[0].zfill(10)
 
-### TRANSFORMATIONS
+### TRANSFORMATIONS - PREPARE INDIVIDUAL DATASETS ###
 # Transform frequentation in train stations
 frequentation = pd.read_csv('data/frequentation-gares-raw.csv', delimiter=";", header=0)
 frequentation["UIC"] = frequentation["UIC"].astype(str).str.zfill(10)
 frequentation = frequentation.rename(columns={'Nom de la gare': 'Gare'})
 
-frequentation.to_csv('data/frequentation-gares.csv', index=False, sep=";")
+frequentation.to_csv('data/generated/frequentation-gares.csv', index=False, sep=";")
 
 
 # Transform wifi presence in train stations
 wifi = pd.read_csv('data/gares-equipees-du-wifi-raw.csv', delimiter=";", header=0)
 wifi["UIC"] = wifi["UIC"].apply(lambda uid: ("87"+str(uid).split(".", maxsplit=1)[0]).zfill(10))
-wifi.to_csv('data/gares-equipees-du-wifi.csv', index=False, sep=";")
+wifi.to_csv('data/generated/gares-equipees-du-wifi.csv', index=False, sep=";")
 
 
+### TRANSFORMATIONS - MERGE DATASETS ###
+FILENAMES = ["data/gares-de-voyageurs.csv",
+             "data/generated/frequentation-gares.csv",
+             "data/gares-pianos.csv",
+             "data/generated/gares-equipees-du-wifi.csv",
+             ]
 
 def merge_data_train_stations(filenames: list[str]) -> pd.DataFrame:
     '''Merge datasets'''
@@ -42,7 +43,44 @@ def merge_data_train_stations(filenames: list[str]) -> pd.DataFrame:
 
     return result
 
-
-# Final merge
+# Final train station merge
 train_stations = merge_data_train_stations(FILENAMES)
+
+# Drop columns
+train_stations = train_stations.drop(columns=['Segmentation DRG'])
+for column in train_stations.columns[10:21]:
+    train_stations = train_stations.drop(columns=[column])
+
+# Convert column names in english
+train_stations = train_stations.rename(columns={
+    'Nom': 'city_name',
+    'UIC': 'uic',
+    'Segment(s) DRG': 'drg_segment',
+    'Position géographique': 'geographical_position',
+    'Code postal': 'postal_code',
+    'Total Voyageurs 2022': 'total_passengers_2022',
+    'Total Voyageurs + Non voyageurs 2022': 'total_passengers_and_non_passengers_2022',
+    'Total Voyageurs 2015': 'total_passengers_2015',
+    'Total Voyageurs + Non voyageurs 2015': 'total_passengers_and_non_passengers_2015',
+    'Piano': 'piano',
+    'Power&Station': 'power_station',
+    'Baby-Foot': 'baby_foot',
+    'Distr Histoires Courtes': 'distr_histoires_courtes',
+    'total': 'total',
+    'Service Wifi': 'wifi_service'
+})
 train_stations.to_csv("sncf_train_stations_dataset.csv", sep=";")
+
+train_stations.rename(columns={'Code commune': 'city_code'}, inplace=True)
+# COnvert city_code to string
+train_stations["city_code"] = train_stations["city_code"].astype(str)
+
+
+### INTEGRATION - CONSOLIDATED CITIES ##
+# Add consolidated cities information
+consolidated_cities = pd.read_csv('data/generated/consolidated-cities.csv', delimiter=";", header=0)
+
+# Merge with train stations
+result = pd.merge(train_stations, consolidated_cities, on="city_code")
+
+result.to_csv('dataset.csv', sep=';')
---
+++ consolidate_cities.py
@@ -0,0 +1,70 @@
+"""
+Consolidates city statistics and attraction area data into a single dataset.
+    Reads two CSV files: 'cities_statistics.csv' and 'attraction_area_per_city.csv'.
+    Performs data cleaning and merging operations on the datasets.
+    Calculates the sum of numerical data per 'Aire d'attraction des villes 2020'.
+    Translates column names to English.
+    Saves the consolidated dataset as 'consolidated-cities.csv'.
+"""
+import pandas as pd
+
+# Load the data
+cities_statistics = pd.read_csv('data/cities_statistics.csv', delimiter=";", header=2)
+cities_attraction = pd.read_csv('data/attraction_area_per_city.csv', delimiter=";", header=0)
+
+# Change column types
+cities_statistics = cities_statistics.convert_dtypes()
+cities_attraction = cities_attraction.convert_dtypes()
+for column in cities_statistics.columns[2:]:
+    cities_statistics[column] = pd.to_numeric(cities_statistics[column], errors='coerce')
+
+# Create common key
+cities_statistics = cities_statistics.rename(columns={'Code': 'city_code'})
+cities_attraction = cities_attraction.rename(columns={'Code géographique': 'city_code'})
+
+# Merge datasets on `city_code`
+result = pd.merge(cities_attraction, cities_statistics, on="city_code")
+
+
+sum_per_attraction = result.groupby("Aire d'attraction des villes 2020").sum()
+
+# Keep only numerical data and 'Aire d'attraction des villes 2020" column
+sum_per_attraction = result.select_dtypes(include='number')
+sum_per_attraction["Aire d'attraction des villes 2020"] = result["Aire d'attraction des villes 2020"]
+sum_per_attraction = sum_per_attraction.drop(columns=["Région",
+                                                      "Catégorie commune dans aire d'attraction des villes 2020"])
+
+# Sum data per `Aire d'attraction des villes 2020`
+sum_per_attraction = sum_per_attraction.groupby("Aire d'attraction des villes 2020").sum()
+
+# Merge with the original dataset
+merged_data = pd.merge(cities_attraction,
+                       sum_per_attraction,
+                       on="Aire d'attraction des villes 2020",
+                       suffixes=('', '_sum'))
+
+# Translate column names to english
+merged_data = merged_data.rename(columns={
+    'Code géographique': 'city_code',
+    'Libellé géographique': 'city_label',
+    'Libellé aire d\'attraction des villes 2020': 'city_attraction_label',
+    'Aire d\'attraction des villes 2020': 'city_attraction_area',
+    'Département': 'department',
+    'Région': 'region',
+    'Population municipale 2021': 'sum_municipal_population_2021',
+    'Nb de pers. non scolarisées de 15 ans ou + 2020': 'non_scholarized_15_years_old_or_more_2020',
+    'Résidences principales 2020': 'main_residences_2020',
+    'Logements 2020': 'housing_2020',
+    'Nb d\'emplois au lieu de travail (LT) 2020': 'jobs_at_workplace_2020',
+    'Nb étab. actifs employeurs au 31/12 2021': 'active_employers_2021',
+    'Nb d\'hotels 2024': 'hotels_2024',
+    'Population municipale 2021_sum': 'municipal_population_2021_sum',
+    'Nb etab. actifs employeurs au 31/12 2021_sum': 'active_employers_2021_sum',
+    'Nb d\'hôtels 2024': 'hotels_2024_sum',
+    'Nb de chambres dans les hôtels 2024': 'hotel_rooms_2024_sum',
+    'Nb de terrains de camping 2024': 'camping_sites_2024_sum',
+    'Nb d\'emplacements de camping 2024': 'camping_sites_2024_sum',
+    'Nb d\'autres hébergements collectifs de tourisme 2024': 'other_tourist_accommodations_2024_sum',
+})
+print(merged_data.dtypes)
+merged_data.to_csv('data/generated/consolidated-cities.csv', sep=';')
