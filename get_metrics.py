6c38ac5406af648d36257cf7efc76543544a98a5
Pierre Jézégou <pierre.jezegou@centrale.centralelille.fr>
2024-05-29 23:29:51 +0200

    Remove model and hyperparams from compute metrics and convert dict to str

--- get_metrics.py
+++ get_metrics.py
@@ -1,21 +1,23 @@
 """Extract petrics from `y_pred` and `y_test` for a given model, and save in csv file."""
+import os
 import pandas as pd
 from sklearn.metrics import r2_score, mean_squared_error, median_absolute_error, mean_absolute_error
 
 def write_metrics_in_csv(y_pred: pd.Series,
-                 y_test: pd.Series,
-                 model_name: str,
-                 file_name: str ='metrics.csv'
-                 ) -> None:
+                         y_real: pd.Series,
+                         model_name: str,
+                         hyperparameters: dict = None,
+                         file_name: str ='metrics.csv'
+                         ) -> None:
     """Write metrics in csv file."""
 
     # Calculating metrics
-    result = compute_metrics(y_pred, y_test, model_name)
+    result = compute_metrics(y_pred, y_real)
 
     # Saving metrics in a DataFrame
     metrics = pd.DataFrame({
         'Model': [model_name],
-        'Hyperparameters': [result['hyperparameters']],
+        'Hyperparameters': [str(hyperparameters)],
         'Mean Squared Error': [result['mse']],
         'Mean Absolute Error': [result['mean_abs_e']],
         'Median Absolute Error': [result['median_abs_e']],
@@ -23,13 +25,11 @@ def write_metrics_in_csv(y_pred: pd.Series,
         })
 
     # Add this new row to the existing csv file
-    metrics.to_csv(file_name, mode='a', header=False, index=False)
+    metrics.to_csv(file_name, mode='a', header=not os.path.exists(file_name), index=False)
 
 
 def compute_metrics(y_pred: pd.Series,
                     y_real: pd.Series,
-                    model_name: str,
-                    hyperparameters: dict = None
                     ) -> dict:
 
     """Compute metrics and save in csv file."""
@@ -39,8 +39,6 @@ def compute_metrics(y_pred: pd.Series,
     mean_abs_e = mean_absolute_error(y_pred, y_real)
 
     result = {
-        'model': model_name,
-        'hyperparameters': hyperparameters,
         'r2': r2,
         'mse': mse,
         'median_abs_e': median_abs_e,
